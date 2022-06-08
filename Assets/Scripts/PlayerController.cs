1f12a2c7b984f9f560df469d15ecc217b53adbc6
DESKTOP-J31KEEU\azir0 <azir0428@gmail.com>
2022-06-08 23:12:44 +0800

    Enemy程式更動?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -525,7 +525,7 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-            timer += Time.unscaledDeltaTime;
+                timer += Time.deltaTime;
             }
         }
     }
