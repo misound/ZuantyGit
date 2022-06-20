2c3f2988b9e64241b4c64144fcb2bcb90bbcae9d
DESKTOP-LTDPVBE\24544 <24544179louis@gmail.com>
2022-06-21 03:03:43 +0800

    把QTE裡的更新日誌刪掉了

--- Assets/Scripts/QTE.cs
+++ Assets/Scripts/QTE.cs
@@ -5,20 +5,6 @@ using UnityEngine.UI;
 
 public class QTE : MonoBehaviour
 {
-    #region 更新日誌 
-    /// 2022/05/27更新日誌 
-    /// 更新了QTE產生機制
-    /// 大要說明為一開始就產生，然後用onTrigger控制開啟和關閉
-    /// 
-
-    /// 2022/05/29更新日誌
-    /// 驗證並把QTE按鈕產生方法改在Enemy上面
-    /// 決定把QTE當資料庫
-    /// 
-    /// 
-    ///2022/06/03更新日誌
-    ///開始編撰資料庫
-    #endregion
 
     public Sprite Sprite_QTEButton;
 
