eddefde5850e26f6f5d1ef5b2e8e7d359f6ba53c
DESKTOP-J31KEEU\azir0 <azir0428@gmail.com>
2022-11-02 21:17:49 +0800

    電梯修正

--- Assets/Scripts/Map/Elevator.cs
+++ Assets/Scripts/Map/Elevator.cs
@@ -57,15 +57,23 @@ public class Elevator : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.transform.SetParent(transform);
-        playerIn = true;
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(transform);
+            playerIn = true;
+        }
+        
 
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        collision.transform.SetParent(null);
-        playerIn = false;
+        if (collision.gameObject.tag=="Player")
+        {
+            collision.transform.SetParent(null);
+            playerIn = false;
+        }
+        
 
     }
 
