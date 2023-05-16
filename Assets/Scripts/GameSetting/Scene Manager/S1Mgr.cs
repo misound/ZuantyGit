1f45f7c2800a9f29fe03c0c15e3e5befca5819e9
DESKTOP-LTDPVBE\24544 <24544179louis@gmail.com>
2023-05-17 00:18:00 +0800

    no message

--- Assets/Scripts/GameSetting/Scene Manager/S1Mgr.cs
+++ Assets/Scripts/GameSetting/Scene Manager/S1Mgr.cs
@@ -67,12 +67,16 @@ public class S1Mgr : MonoBehaviour
             {
                 PlayerHP.SetMaxHealth(GameSetting.PlayerHP = 100); //最高生命值
                 PlayerHP.SetHealth(GameSetting.PlayerHP); //刷新當前血量
+                
+                PlayerHP.BuyPoka();
             }
             else if(GameSetting.PlayerHP > 0) 
             {
                 PlayerHP.SetMaxHealth(GameSetting.PlayerHP = 100); //最高生命值
                 PlayerHP.GetHealth(GameSetting.PlayerHP = PlayerPrefs.GetInt("PlayerHP")); //讀取血量
                 PlayerHP.SetHealth(GameSetting.PlayerHP); //刷新當前血量
+                
+                PlayerHP.GetPoka(GameSetting.Poka);
             }
             SPC.transform.position = GameSetting.Playerpos;
         }
